8a84fdb6d430acc54f035d7a9e2b0fca63973f60
Yamuna Ankamreddy <yamunaankamreddy755@gmail.com>
2026-05-31 21:49:35 +0530

    fixed the bugs

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,8 +1,10 @@
 @echo off
+setlocal
 set MAVEN_WRAPPER_DIR=.mvn\wrapper
+set MAVEN_PROJECTBASEDIR=%~dp0
 if defined JAVA_HOME (
-  set JAVA_CMD=%JAVA_HOME%\bin\java
+  set "JAVA_CMD=%JAVA_HOME%\bin\java"
 ) else (
-  set JAVA_CMD=java
+  set "JAVA_CMD=java"
 )
-%JAVA_CMD% -jar "%~dp0%MAVEN_WRAPPER_DIR%\maven-wrapper.jar" %*
+"%JAVA_CMD%" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" -cp "%MAVEN_PROJECTBASEDIR%%MAVEN_WRAPPER_DIR%\maven-wrapper.jar" org.apache.maven.wrapper.MavenWrapperMain %*
